2ed2047a48fa1a1a0d8f1347842d2f248cfbe3ea
paulobtn <paulonetodev@gmail.com>
2021-08-19 19:23:30 -0300

    finishing fist version of the api

--- src/repos/source-repo.js
+++ src/repos/source-repo.js
@@ -11,21 +11,47 @@ class SourceRepo {
   
   //get a particular source
   static async findById(id){
-    const {rows} = await pool.query('SELECT * FROM source WHERE id = $1 ', [id]) 
+    const {rows} = await pool.query('SELECT * FROM source WHERE id = $1;', [id]) 
 
     return toCamelCase(rows)[0];
   }
   
-  static async insert(){
+  static async insert(language, project, uri, src){
+    const {rows} = await pool.query(
+      'INSERT INTO source (language, project, uri, src) VALUES ($1, $2, $3, $4) RETURNING *;',
+      [language, project, uri, src]
+    );
+
+    return toCamelCase(rows)[0];
+  }
+
+  static async update(id, language, project, uri, src){
+
+    const {
+      rows,
+    } = await pool.query(
+      'UPDATE source SET language = $1, project = $2, uri = $3, src = $4 WHERE id = $5 RETURNING *;',
+      [language, project, uri, src, id]
+    );
+
+    return toCamelCase(rows)[0];
 
   }
 
-  static async update(){
+  static async delete(id){
+    
+    const {
+      rows,
+    } = await pool.query('DELETE FROM source WHERE id = $1 RETURNING *;', [id]);
 
+    return toCamelCase(rows)[0];
   }
 
-  static async delete(){
 
+  static async count() {
+    const { rows } = await pool.query('SELECT COUNT(*) FROM source;');
+
+    return parseInt(rows[0].count);
   }
 }
 
--- src/routes/source.js
+++ src/routes/source.js
@@ -6,8 +6,13 @@ const router = express.Router();
 router.get('/source', async (req, res) => {
   //get all the source codes
   const sources = await SourceRepo.find();
+  
+  if(sources){
+    res.send(sources);
+  } else{
+    res.sendStatus(404);
+  }
 
-  res.send(sources);
 });
 
 router.get('/source/:id', async (req, res) => {
@@ -24,15 +29,37 @@ router.get('/source/:id', async (req, res) => {
 });
 
 router.post('/source', async (req, res) => {
+  const { language, project, uri, src } = req.body;
 
+  const source = await SourceRepo.insert(language, project, uri, src);
+
+  res.send(source);
 });
 
 router.put('/source/:id', async (req, res) => {
+  const {id} = req.params;
+  const {language, project, uri, src } = req.body;
 
+  const source = await SourceRepo.update(id, language, project, uri, src);
+
+  if(source){
+    res.send(source);
+  } else{
+    res.sendStatus(404);
+  }
 });
 
 router.delete('/source/:id', async (req, res) => {
 
+  const { id } = req.params;
+
+  const source = await SourceRepo.delete(id);
+
+  if (source) {
+    res.send(source);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 module.exports = router;
