4006450b2f778964c90f38c97bc45763dd470915
paulobtn <paulonetodev@gmail.com>
2021-08-21 11:25:20 -0300

    fixing typo

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {response.data && <p>
-         {response.data}
+         {response.data.id}
         </p>}
       </header>
     </div>
