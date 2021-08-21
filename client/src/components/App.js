da2287dd89c0315d42d9abcc66fd516ba52f072f
paulobtn <paulonetodev@gmail.com>
2021-08-21 11:55:25 -0300

    embedded the ace text editor

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,7 @@
 import './style/App.css';
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/theme-github";
 
 import useRequest from '../hooks/useRequest';
 
@@ -10,12 +13,50 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {response.data && <p>
-         {response.data.id}
-        </p>}
+      
+    {response.data && 
+      <AceEditor
+        mode="c_cpp"
+        theme="github"
+        onChange={(a) => {console.log(a)}}
+        value={response.data.src}
+        name="div-id"
+        editorProps={{ $blockScrolling: true }}
+      />
+      }
+      
+
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+// import React from "react";
+// import { render } from "react-dom";
+// import AceEditor from "react-ace";
+
+// import "ace-builds/src-noconflict/mode-java";
+// import "ace-builds/src-noconflict/theme-github";
+
+// function onChange(newValue) {
+  // console.log("change", newValue);
+// }
+
+// // Render editor
+// render(
+  // <AceEditor
+    // mode="java"
+    // theme="github"
+    // onChange={onChange}
+    // name="UNIQUE_ID_OF_DIV"
+    // editorProps={{ $blockScrolling: true }}
+  // />,
+  // document.getElementById("example")
+// );
