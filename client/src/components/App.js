243a640d8d495b6bf768e988f1b5274271fed592
paulobtn <paulonetodev@gmail.com>
2021-08-21 11:57:22 -0300

    removed comments

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -32,31 +32,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-// import React from "react";
-// import { render } from "react-dom";
-// import AceEditor from "react-ace";
-
-// import "ace-builds/src-noconflict/mode-java";
-// import "ace-builds/src-noconflict/theme-github";
-
-// function onChange(newValue) {
-  // console.log("change", newValue);
-// }
-
-// // Render editor
-// render(
-  // <AceEditor
-    // mode="java"
-    // theme="github"
-    // onChange={onChange}
-    // name="UNIQUE_ID_OF_DIV"
-    // editorProps={{ $blockScrolling: true }}
-  // />,
-  // document.getElementById("example")
-// );
