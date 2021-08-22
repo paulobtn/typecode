e793aec556cf08a6472bf8e5c9112dff7d2eb6f7
paulobtn <paulonetodev@gmail.com>
2021-08-22 14:22:38 -0300

    displaying random code at the screen

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,16 +4,30 @@ import useRequest from '../hooks/useRequest';
 
 function App() {
 
+  const renderCode = (src) => {
+    
+    return (
+      src.split("").map(character => {
+        return (
+            <span className="src-char">{character}</span>
+        )
+      })
+    )
+  }
+
   const response = useRequest('/api/source/random');
   console.log(response);
 
+
   return (
     <div className="App">
-      <header className="App-header">
-      
-        <p>React App</p> 
 
-      </header>
+    <div className="source-container">
+      <code>
+        {response.data && renderCode(response.data.src)}
+      </code>
+    </div>
+
     </div>
   );
 }
