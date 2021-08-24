8bce996e57247b48a1eeef3299e281a51d4198c2
paulobtn <paulonetodev@gmail.com>
2021-08-24 16:56:41 -0300

    made the code more readable

--- client/src/components/GameLogic.js
+++ client/src/components/GameLogic.js
@@ -21,6 +21,10 @@ const forbiddenKeys = [
   'Escape'
 ]
 
+const GAME_STATE_IDLE    = 'GAME_STATE_IDLE';
+const GAME_STATE_RUNNING = 'GAME_STATE_RUNNING';
+const GAME_STATE_END   = 'GAME_STATE_END';
+
 const GameLogic = (props) => {
 
   const [codeState, setCodeState] = useState({
@@ -29,100 +33,65 @@ const GameLogic = (props) => {
     startTime: 0,
     endTime: 0,
     pressedTime: 0,
-    timeFromPrevious: 0
+    gameState: GAME_STATE_IDLE
   });
 
-  // const [gameTimer, setGameTimer] = useState({
-    // start: 0,
-    // end: 0
-  // })
-
-
-  //constrols input and updates the codeState 
+  //controls input and updates the codeState 
   useEffect(() => {
    
     const listener = (event) => {
 
       let pressedKey = convertTypedCharacter(event.key);
       
-      
       if(props.src === null) return;
       if(forbiddenKeys.includes(pressedKey)) return;
       if(pressedKey === '\t') event.preventDefault();
       
       //checks if the key is correct and updates the cursor
-
       setCodeState(prev => {
 
-        let newTime = {
-                        startTime: prev.startTime,
-                        endTime: prev.endTime,
-                        pressedTime: prev.pressedTime,
-                        timeFromPrevious: prev.timeFromPrevious
-                      };
-
+        let newCodeState = {...prev};
 
         //current time
         let currTime = Date.now();
 
-        //starts the timer if it's the first key
+        //starts the timer and the game if it's the first key
         if(prev.cursorPosition === 0){
           //the game just started
-          newTime.startTime = currTime;
-          newTime.pressedTime = currTime;
-          newTime.timeFromPrevious = 0;
+          newCodeState.startTime = currTime;
+          newCodeState.pressedTime = currTime;
+          newCodeState.gameState = GAME_STATE_RUNNING;
         }
-
-        //calculates the time from the previous right character
-        newTime.timeFromPrevious = currTime - prev.pressedTime;
 
         //if the key is correct, updates the cursor and time values
         if(props.src[prev.cursorPosition] === pressedKey){
 
-          let newCursorPosition = prev.cursorPosition + 1;
+          newCodeState.pressedTime = currTime;
+          newCodeState.cursorPosition = prev.cursorPosition + 1;
           
           //check if the user typed everything
-          if(newCursorPosition === props.src.length){
-            newTime.endTime = currTime;
+          if(newCodeState.cursorPosition === props.src.length){
+            newCodeState.endTime = currTime;
+            newCodeState.gameState = GAME_STATE_END;
           }
-
-          newTime.pressedTime = currTime;
-          return {
-            ...prev, 
-            cursorPosition: newCursorPosition,
-
-            startTime: newTime.startTime,
-            endTime: newTime.endTime,
-            pressedTime: newTime.pressedTime,
-            timeFromPrevious: newTime.timeFromPrevious
-          }
+          
+          return newCodeState;
         }
 
         //beyond that line, the key is incorrect
-        //user already pressed wrong, don't change cursor values, only update the time
+        //user already pressed wrong, don't change cursor values
         let newWrongChars = prev.wrongChars;
         if(newWrongChars.length > 0 &&
            newWrongChars[newWrongChars.length - 1] === prev.cursorPosition ){
           
-          return { ...prev,
-                   startTime: newTime.startTime,
-                   endTime: newTime.endTime,
-                   pressedTime: newTime.pressedTime,
-                   timeFromPrevious: newTime.timeFromPrevious
-                 };
+          return newCodeState;
         }
         
         //user just typed something wrong, add new entry to the wrong keys array
         newWrongChars.push(prev.cursorPosition);
-
-        return{
-          cursorPosition: prev.cursorPosition,
-          wrongChars: newWrongChars,
-          startTime: newTime.startTime,
-          endTime: newTime.endTime,
-          pressedTime: newTime.pressedTime,
-          timeFromPrevious: newTime.timeFromPrevious
-        }
+        newCodeState.wrongChars = newWrongChars;
+        
+        return newCodeState;
       })
     }
 
