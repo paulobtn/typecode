275ddd7ce7b668f2bb0d5791b6f5019638e376d2
paulobtn <paulonetodev@gmail.com>
2021-08-25 16:58:43 -0300

    improved the wpm timer, now it starts and stops based on the game state

--- client/src/components/ScoreWPM.js
+++ client/src/components/ScoreWPM.js
@@ -19,31 +19,62 @@ const ScoreWPM = (props) => {
 
 
   const [scoreDisplay, setScoreDisplay] = useState({
-    isActive: true,
+    timerIsActive: false,
     counter: 0,
     wpm: 0
   });
-  
+
+  const setTimer = (value) => {
+    setScoreDisplay( (prev) => {
+        return (
+          {
+            ...prev,
+            timerIsActive: value,
+            counter: 0
+          }
+        )
+      }
+    );
+  }
+
   //updates score every half second
   useEffect (() => {
     let interval;
     
-    if(scoreDisplay.isActive && props.codeState.gameState === 'GAME_STATE_RUNNING'){
+    // if the game is running and timer is active
+    if(scoreDisplay.timerIsActive){
       interval = setInterval(() => {
+
+        //increment timer
         setScoreDisplay( (prev) => {
           return {...prev, counter: prev.counter + 1, wpm: getWPM(props.codeState)};
         });
 
-        console.log(scoreDisplay.counter);
       }, 500);
     }
+    
 
     return () => clearInterval(interval);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [scoreDisplay, props.codeState.gameState]);
+  
+  //starts or stops timer based on the game state
+  useEffect (() => {
+    switch(props.codeState.gameState) {
+      case 'GAME_STATE_END':
+      // [> falls through <]
+      case 'GAME_STATE_IDLE':
+        setTimer(false);
+        break;
+      case 'GAME_STATE_RUNNING':
+        setTimer(true);
+        break;
+      default:
+    }
+  }, [props.codeState.gameState])
 
-
+  console.log(scoreDisplay);
   return (
     <div className="wpm-container">
       <span className="wpm-value">
